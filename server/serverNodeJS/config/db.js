aa168a915489611ba59fb2acac3cd3b44b6453a4
Duy Le <lehongduy.bk@gmail.com>
2024-04-04 16:36:46 +0700

    update code

--- server/serverNodeJS/config/db.js
+++ server/serverNodeJS/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// code 4/4/2024
 const local = "mongodb+srv://duylh17:SCccEuvJPCG6M9LZ@cluster0.0n8qgpd.mongodb.net/md18303";
 
 const connect = async () => {
